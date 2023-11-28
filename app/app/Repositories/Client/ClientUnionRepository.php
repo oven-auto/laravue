3173e3ba3bdffa7ff07e30ffcbec40c8af8c8294
oven-auto <oit@oven-auto.ru>
2023-11-28 16:26:28 +0300

    union

--- app/app/Repositories/Client/ClientUnionRepository.php
+++ app/app/Repositories/Client/ClientUnionRepository.php
@@ -25,22 +25,23 @@ class ClientUnionRepository
      */
     public function addUnion(Client $client, int $unionClientId) : void
     {
-        if($unionClientId == $client->id)
-            throw new \Exception('Клиент к которму Вы хотите добавить связь не может быть тем же кого добавляете');
+        // if($unionClientId == $client->id)
+        //     throw new \Exception('Клиент к которму Вы хотите добавить связь не может быть тем же кого добавляете');
 
         $collect = $client->unionsParent->merge($client->unionsChildren);
 
-        if($collect->contains('id', $unionClientId))
-            throw new \Exception('Связь уже есть');
+        // if($collect->contains('id', $unionClientId))
+        //     throw new \Exception('Связь уже есть');
+        if(!$collect->contains('id', $unionClientId))
+        {
+            $unions = $client->unionsChildren->pluck('id')->toArray();
+            array_push($unions, $unionClientId);
+            $unions = array_unique($unions);
 
-        $unions = $client->unionsChildren->pluck('id')->toArray();
-        array_push($unions, $unionClientId);
-        $unions = array_unique($unions);
+            $client->unionsChildren()->sync($unions);
 
-        $client->unionsChildren()->sync($unions);
-
-        $client = $client->fresh();
-
+            $client = $client->fresh();
+        }
         Comment::add(ClientUnion::fillData($client->id, $unionClientId), 'create');
     }
 
