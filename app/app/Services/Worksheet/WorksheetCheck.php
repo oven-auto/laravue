bf8901102294501d5e023c141f84e1df32de64a0
oven-auto <oit@oven-auto.ru>
2023-12-11 12:49:27 +0300

    FixCheckWorksheet

--- app/app/Services/Worksheet/WorksheetCheck.php
+++ app/app/Services/Worksheet/WorksheetCheck.php
@@ -12,11 +12,20 @@ Class WorksheetCheck
     {
         $worksheets = $client->open_worksheets;
         $worksheetExistArray = [];
+        // dump([
+        //     'trafic_company' => $trafic->company_id,
+        //     'trafic_structure' => $trafic->structure->id,
+        // ]);
+
         foreach ($worksheets as $item)
         {
+            // dump([
+            //     'ws_company_id' => $item->company_id,
+            //     'ws_stucture' => $item->structure_id,
+            // ]);
             if(
                 //$item->appeal_id == $trafic->appeal_id &&
-                $item->structure_id == $trafic->company_structure_id &&
+                $item->structure_id == $trafic->structure->id &&
                 $item->company_id == $trafic->company_id
             )
             {
