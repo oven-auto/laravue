39420910dbd08aea62c38b4d580d807ced8afe1f
oit <oit@oven-auto.ru>
2024-11-22 10:09:14 +0300

    Wait

--- app/app/Classes/Wait/Wait.php
+++ app/app/Classes/Wait/Wait.php
@@ -14,8 +14,10 @@ Class Wait
         if($count > 0 && $count < 3)
             sleep(0);
         elseif($count > 3 && $count < 6)
-            sleep(1);
-        elseif($count > 6)
             sleep(2);
+        elseif($count > 6 && $count < 9)
+            sleep(3);
+        elseif($count > 9)
+            sleep(4);
     }
 }
