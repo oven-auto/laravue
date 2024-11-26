440dcf84c29d444b634293d5080bfde115b10664
oit <oit@oven-auto.ru>
2024-11-26 12:26:40 +0300

    alert

--- app/app/Classes/Wait/Wait.php
+++ app/app/Classes/Wait/Wait.php
@@ -11,15 +11,23 @@ Class Wait
     public static function setWaitColor()
     {
         $count = DealerColorImage::select(DB::raw('count(id) as count'))->first()->toArray()['count'];
-
+        $sleep = 0;
         if($count > 0 && $count < 3)
-            sleep(0);
+            $sleep = 1;
         elseif($count > 3 && $count < 6)
-            sleep(3);
+            $sleep = 3;
         elseif($count > 6 && $count < 9)
-            sleep(4);
-        elseif($count > 9)
-            sleep(5);
-        Log::alert('Внимание Большая Задержка');
+            $sleep = 4;
+        elseif($count > 9 && $count < 12)
+            $sleep = 5;
+        elseif($count > 12 && $count < 15)
+            $sleep = 6;
+        elseif($count > 15 && $count < 18)
+            $sleep = 7;
+        elseif($count > 15)
+            $sleep = 8;
+
+        sleep($sleep);
+        Log::alert('Внимание Большая Задержка'. $sleep);
     }
 }
