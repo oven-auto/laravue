ee8956f1b07e8bf018f5cdbc38dbdd54b441f922
oit <oit@oven-auto.ru>
2024-12-06 16:07:07 +0300

    test

--- app/app/Http/Requests/Client/ClientStoreRequest.php
+++ app/app/Http/Requests/Client/ClientStoreRequest.php
@@ -83,7 +83,7 @@ class ClientStoreRequest extends FormRequest
             'driving_license' => 'nullable|regex:([0-9]{4}\s{1}[0-9]{6})',
             'serial_number' => 'nullable|regex:([0-9]{4}\s{1}[0-9]{6})',
             'contacts' => 'array|required',
-            'form_owner_id' => 'sometimes|numeric',
+            'form_owner_id' => 'sometimes|numeric|nullable',
 
             'contacts.*.phone' => [
                 'distinct',
