280952da847dbe88bf1484f399a5eb5d0fbc203c
oit <oit@oven-auto.ru>
2025-01-09 16:05:25 +0300

    Консольная команда, для управления событиями в DNM

---
+++ app/app/Console/Commands/DNMCONTROLL.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Classes\LadaDNM\DNMAppealService;
+use App\Classes\LadaDNM\DNMClientService;
+use App\Classes\LadaDNM\DNMEvent;
+use App\Classes\LadaDNM\DNMWorksheetService;
+use App\Models\Client;
+use App\Models\WsmReserveNewCar;
+use Illuminate\Console\Command;
+
+class DNMCONTROLL extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dnm:control';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $q = 'Укажите ID резерва';
+
+        $id = $this->ask($q);
+
+        $reserve = WsmReserveNewCar::withTrashed()->find($id);
+        
+        if(!$reserve)
+            exit('Резерв не найден');
+
+        print_r([
+            '1' => 'Создать/Изменить клиента',
+            '2' => 'Создать/Изменить РЛ',
+            '3' => 'Создать/Изменить потребность',
+            '4' => 'Создать/Изменить событие',
+        ]);
+
+        $action = $this->ask('Укажи номер действия');
+
+        match($action) {
+            '1' => $this->clientSend($reserve),
+            '2' => $this->worksheetSend($reserve),
+            '3' => $this->appealSend($reserve),
+            '4' => $this->eventSend($reserve),
+            default => '',
+        };
+    }
+
+
+
+    public function clientSend(WsmReserveNewCar $reserve)
+    {
+        (new DNMClientService())->save($reserve);
+    }
+
+
+
+    public function worksheetSend(WsmReserveNewCar $reserve)
+    {
+        (new DNMWorksheetService())->save($reserve->worksheet);
+    }
+
+
+
+    public function appealSend(WsmReserveNewCar $reserve)
+    {
+        (new DNMAppealService())->save($reserve);
+    }
+
+
+
+    public function eventSend(WsmReserveNewCar $reserve)
+    {
+        print_r([
+            'visit'         => 'Визит',
+            'reject'        => 'Отмена',
+            'call'          => 'Звонок',
+            'testdrive'     => 'ТестДрайв',
+            'offer'         => 'Оффер',
+            'internet'      => 'Интернет запрос',
+            'contract'      => 'Контракт',
+            'issue'         => 'Выдача',
+        ]);
+
+        $action = $this->ask('Введите действие');
+
+        (new DNMEvent())->handler($reserve, $action);
+    }
+}
