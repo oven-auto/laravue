a8c3245f1ca66dec1f50a96b8c9773afbac55525
oit <oit@oven-auto.ru>
2025-05-21 17:21:27 +0300

    trafAn

--- app/app/Services/Analytic/AbstractClasses/AbstractReport.php
+++ app/app/Services/Analytic/AbstractClasses/AbstractReport.php
@@ -16,12 +16,13 @@ abstract class AbstractReport
         {
             $date_1 = Carbon::createFromFormat('d.m.Y', $item[0])
                 ->setHour(0)
-                ->setMinute(0);
+                ->setMinute(0)
+                ->setSecond(0);
 
             $date_2 = isset($item[1]) ? Carbon::createFromFormat('d.m.Y', $item[1]) : $date_1;
-            $date_2->setHour(23)->setMinute(59);
+            $date_2->setHour(23)->setMinute(59)->setSecond(59);
 
-            $arr[] = [$date_1->format('Y-m-d H:i'), $date_2->format('Y-m-d H:i')];
+            $arr[] = [$date_1->format('Y-m-d H:i:s'), $date_2->format('Y-m-d H:i:s')];
         }
         
         return $arr;
