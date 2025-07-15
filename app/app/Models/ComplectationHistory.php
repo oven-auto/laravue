f1c77b26f08a549a1b3561bfeb43356ef5c6f842
oit <oit@oven-auto.ru>
2025-07-15 17:58:52 +0300

    ttt

--- app/app/Models/ComplectationHistory.php
+++ app/app/Models/ComplectationHistory.php
@@ -13,6 +13,6 @@ class ComplectationHistory extends Model
 
     public function author()
     {
-        return $this->hasOne(\App\Models\User::class, 'id', 'author_id')->withTrashed();
+        return $this->hasOne(\App\Models\User::class, 'id', 'author_id')->withTrashed()->withDefault();
     }
 }
