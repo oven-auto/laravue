bc0401ff8bca3fca5523192941e9232cfbf6934c
oit <oit@oven-auto.ru>
2025-07-15 17:02:27 +0300

    ttt

--- app/app/Services/Analytic/Report/ReportService.php
+++ app/app/Services/Analytic/Report/ReportService.php
@@ -25,11 +25,11 @@ Class ReportService
                 'car' => function($q){
                     $q->with([
                         'mark',
-                        //'complectation.current_price',
-                        //'options.current_price',
+                        'complectation.current_price',
+                        'options.current_price',
                         'collector',
-                        //'priority.sale_priority',
-                        //'logistic_dates',
+                        'priority.sale_priority',
+                        'logistic_dates',
                     ]);
                 },
                 'payments',
